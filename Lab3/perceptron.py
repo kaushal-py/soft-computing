ef4e28fbf334f1c8b725ae1d404757b0ebc0275a
kb-studios <2015kaushal.bhogale@ves.ac.in>
2019-02-28 17:30:52 +0530

    Define the methods for perceptron

---
+++ Lab3/perceptron.py
@@ -0,0 +1,22 @@
+class Perceptron():
+
+    def __init__(self):
+        pass
+    
+    def _activate(self):
+        pass
+    
+    def _forward(self):
+        pass
+    
+    def _update_weights(self):
+        pass
+    
+    def _run_epoch(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def output_weights(self):
+        pass
